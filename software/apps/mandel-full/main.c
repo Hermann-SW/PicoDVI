b793075e44ce3684a72ed3d1c1f11a1a9309f4d1
Mike Bell <mdb036@gmail.com>
2021-03-06 16:15:46 +0000

    Mandelbrot full res using palette encoder

--- software/apps/mandel-full/main.c
+++ software/apps/mandel-full/main.c
@@ -36,114 +36,35 @@
 
 uint8_t mandel[FRAME_WIDTH * (FRAME_HEIGHT / 2)];
 
-#define PALETTE_SIZE 64
+#define PALETTE_BITS 6
+#define PALETTE_SIZE (1 << PALETTE_BITS)
 uint16_t palette[PALETTE_SIZE];
 
-uint32_t tmds_palette_blue[PALETTE_SIZE * 2];
-uint32_t tmds_palette_green[PALETTE_SIZE * 2];
-uint32_t tmds_palette_red[PALETTE_SIZE * 2];
+uint32_t tmds_palette[PALETTE_SIZE * 6];
 
 struct dvi_inst dvi0;
 struct semaphore dvi_start_sem;
 
 FractalBuffer fractal;
 
-// Just dumped out register values from running the code in tmds_encode.c:
-// channel_msb: 4, lshift_lower: 3, i0 ctrl: 02801c60, 00002117, i1 ctrl: 02801c6d, 00002117
-// channel_msb: 10, lshift_lower: 0, i0 ctrl: 02801c43, 00002117, i1 ctrl: 02801c53, 00002117
-// channel_msb: 15, lshift_lower: 0, i0 ctrl: 02801c68, 00002117, i1 ctrl: 00001c78, 00002117
-//
-// This saves a few cycles vs doing the setup dynamically (just enough to tip
-// us over the edge as it happens, when proper DC balancing is enabled.)
-
-extern uint32_t tmds_table_fullres_x[];
-extern uint32_t tmds_table_fullres_y[];
-
-static inline void init_interp_for_encode() {
-  interp0_hw->ctrl[1] = 0x00002117u;
-  interp1_hw->ctrl[1] = 0x00002117u;
-  uint32_t lutbase = (uint32_t)(get_core_num() ? tmds_table_fullres_x : tmds_table_fullres_y);
-  interp0_hw->base[2] = (uint32_t)lutbase;
-  interp1_hw->base[2] = (uint32_t)lutbase;
-}
-
-static inline void prepare_scanline_core1(const uint32_t *colourbuf, uint32_t *tmdsbuf) {
-  const uint pixwidth = 640;
-#if 0
-  // Blue
-  interp0_hw->ctrl[0] = 0x02801c60u;
-  interp1_hw->ctrl[0] = 0x02801c6du;
-  tmds_fullres_encode_loop_16bpp_leftshift_x(colourbuf, tmdsbuf, pixwidth, 3);
-  // Green
-  interp0_hw->ctrl[0] = 0x02801c43u;
-  interp1_hw->ctrl[0] = 0x02801c53u;
-  tmds_fullres_encode_loop_16bpp_x(colourbuf, tmdsbuf + pixwidth, pixwidth);
-  // Red
-  interp0_hw->ctrl[0] = 0x02801c68u;
-  interp1_hw->ctrl[0] = 0x00001c78u;
-  tmds_fullres_encode_loop_16bpp_x(colourbuf, tmdsbuf + 2 * pixwidth, pixwidth);
-#else
-  interp0_hw->base[2] = (uint32_t)tmds_palette_blue;
-  interp1_hw->base[2] = (uint32_t)tmds_palette_blue;
-  interp0_hw->ctrl[0] = 0x02801c40u;
-  interp1_hw->ctrl[0] = 0x02801c48u;
-  tmds_fullres_encode_loop_16bpp_x(colourbuf, tmdsbuf, pixwidth);
-
-  interp0_hw->base[2] = (uint32_t)tmds_palette_green;
-  interp1_hw->base[2] = (uint32_t)tmds_palette_green;
-  interp0_hw->ctrl[0] = 0x02801c40u;
-  interp1_hw->ctrl[0] = 0x02801c48u;
-  tmds_fullres_encode_loop_16bpp_x(colourbuf, tmdsbuf + pixwidth, pixwidth);
-
-  interp0_hw->base[2] = (uint32_t)tmds_palette_red;
-  interp1_hw->base[2] = (uint32_t)tmds_palette_red;
-  interp0_hw->ctrl[0] = 0x02801c40u;
-  interp1_hw->ctrl[0] = 0x02801c48u;
-  tmds_fullres_encode_loop_16bpp_x(colourbuf, tmdsbuf + pixwidth * 2, pixwidth);
-#endif
-
-}
-
-static inline void prepare_scanline_core0(const uint32_t *colourbuf, uint32_t *tmdsbuf) {
-  const uint pixwidth = 640;
-  interp0_hw->base[2] = (uint32_t)tmds_palette_blue;
-  interp1_hw->base[2] = (uint32_t)tmds_palette_blue;
-  interp0_hw->ctrl[0] = 0x02801c40u;
-  interp1_hw->ctrl[0] = 0x02801c48u;
-  tmds_fullres_encode_loop_16bpp_y(colourbuf, tmdsbuf, pixwidth);
-
-  interp0_hw->base[2] = (uint32_t)tmds_palette_green;
-  interp1_hw->base[2] = (uint32_t)tmds_palette_green;
-  interp0_hw->ctrl[0] = 0x02801c40u;
-  interp1_hw->ctrl[0] = 0x02801c48u;
-  tmds_fullres_encode_loop_16bpp_y(colourbuf, tmdsbuf + pixwidth, pixwidth);
-
-  interp0_hw->base[2] = (uint32_t)tmds_palette_red;
-  interp1_hw->base[2] = (uint32_t)tmds_palette_red;
-  interp0_hw->ctrl[0] = 0x02801c40u;
-  interp1_hw->ctrl[0] = 0x02801c48u;
-  tmds_fullres_encode_loop_16bpp_y(colourbuf, tmdsbuf + pixwidth * 2, pixwidth);
-}
+static uint8_t palette_offset = 0;
 
 void init_palette() {
-  for (int i = 0; i < PALETTE_SIZE; ++i) {
-    if (i < 0x20) palette[i] = i;
-    else if (i < 0x60) palette[i] = (i - 0x20) << 5;
-    else if (i < 0xc0) palette[i] = ((i - 0x60) >> 2) << 11;
-    else palette[i] = ((i - 0xc0) >> 1) * 0x0840;
+  palette[0] = 0;
+  for (int i = 1; i < PALETTE_SIZE; ++i) {
+    uint8_t c = i + palette_offset;
+    if (c < 0x20) palette[i] = c;
+    else if (c < 0x40) palette[i] = (c - 0x20) << 6;
+    else if (c < 0x60) palette[i] = (c - 0x40) << 11;
+    else if (c < 0x80) palette[i] = ((c - 0x60) & 0x1f) * 0x0840;
+    else if (c < 0xa0) palette[i] = ((c - 0x80) & 0x1f) * 0x0041;
+    else if (c < 0xc0) palette[i] = ((c - 0xa0) & 0x1f) * 0x0801;
+    else if (c < 0xe0) palette[i] = ((c - 0xc0) & 0x1f) * 0x0841;
+    else palette[i] = 0;
   }
+  ++palette_offset;
 
-  for (int i = 0; i < PALETTE_SIZE; ++i) {
-    uint16_t blue = (palette[i] << 1) & 0x3e;
-    uint16_t green = (palette[i] >> 5) & 0x3f;
-    uint16_t red = (palette[i] >> 10) & 0x3e;
-    tmds_palette_blue[i] = tmds_table_fullres_x[blue];
-    tmds_palette_blue[i + PALETTE_SIZE] = tmds_table_fullres_x[64 + blue];
-    tmds_palette_green[i] = tmds_table_fullres_x[green];
-    tmds_palette_green[i + PALETTE_SIZE] = tmds_table_fullres_x[64 + green];
-    tmds_palette_red[i] = tmds_table_fullres_x[red];
-    tmds_palette_red[i + PALETTE_SIZE] = tmds_table_fullres_x[64 + red];
-  }
+  tmds_setup_palette_symbols(palette, tmds_palette, PALETTE_SIZE);
 }
 
 void init_mandel() {
@@ -178,6 +99,8 @@ void zoom_mandel() {
     return;
   }
 
+  printf("Zoom: %ld\n", zoom_count);
+
   float zoomx = -.75f - .56f * ((float)zoom_count / (float)NUM_ZOOMS);
   float sizex = fractal.maxx - fractal.minx;
   float sizey = fractal.miny * -2.f;
@@ -196,12 +119,10 @@ void __not_in_flash("core1_main") core1_main() {
   sem_acquire_blocking(&dvi_start_sem);
   dvi_start(&dvi0);
 
-  init_interp_for_encode();
-
   while (1) {
     const uint32_t *colourbuf = (const uint32_t*)multicore_fifo_pop_blocking();
     uint32_t *tmdsbuf = (uint32_t*)multicore_fifo_pop_blocking();
-    prepare_scanline_core1(colourbuf, tmdsbuf);
+    tmds_encode_palette_data((const uint32_t*)colourbuf, tmds_palette, tmdsbuf, FRAME_WIDTH, PALETTE_BITS);
     multicore_fifo_push_blocking(0);
   }
   __builtin_unreachable();
@@ -234,8 +155,6 @@ int __not_in_flash("main") main() {
 
   uint heartbeat = 0;
 
-  init_interp_for_encode();
-
   sem_release(&dvi_start_sem);
   while (1) {
     if (++heartbeat >= 30) {
@@ -243,6 +162,7 @@ int __not_in_flash("main") main() {
       gpio_xor_mask(1u << LED_PIN);
     }
     if (fractal.done) zoom_mandel();
+    if (heartbeat & 1) init_palette();
     for (int y = 0; y < FRAME_HEIGHT / 2; y += 2) {
       uint32_t *our_tmds_buf, *their_tmds_buf;
       queue_remove_blocking_u32(&dvi0.q_tmds_free, &their_tmds_buf);
@@ -250,7 +170,7 @@ int __not_in_flash("main") main() {
       multicore_fifo_push_blocking((uint32_t)their_tmds_buf);
   
       queue_remove_blocking_u32(&dvi0.q_tmds_free, &our_tmds_buf);
-      prepare_scanline_core0((const uint32_t*)(&mandel[(y+1)*FRAME_WIDTH]), our_tmds_buf);
+      tmds_encode_palette_data((const uint32_t*)(&mandel[(y+1)*FRAME_WIDTH]), tmds_palette, our_tmds_buf, FRAME_WIDTH, PALETTE_BITS);
       
       multicore_fifo_pop_blocking();
 
@@ -266,7 +186,7 @@ int __not_in_flash("main") main() {
       multicore_fifo_push_blocking((uint32_t)their_tmds_buf);
   
       queue_remove_blocking_u32(&dvi0.q_tmds_free, &our_tmds_buf);
-      prepare_scanline_core0((const uint32_t*)(&mandel[y*FRAME_WIDTH]), our_tmds_buf);
+      tmds_encode_palette_data((const uint32_t*)(&mandel[y*FRAME_WIDTH]), tmds_palette, our_tmds_buf, FRAME_WIDTH, PALETTE_BITS);
       
       multicore_fifo_pop_blocking();
 
