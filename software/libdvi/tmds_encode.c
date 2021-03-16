bd0ebbb5ad4e1ffdcbc186be1ee93ef29c5c22c6
Mike Bell <mike@mercuna.com>
2021-03-16 22:16:56 +0000

    Improve symbol encoding

--- software/libdvi/tmds_encode.c
+++ software/libdvi/tmds_encode.c
@@ -167,26 +167,18 @@ void __not_in_flash_func(tmds_encode_data_channel_fullres_16bpp)(const uint32_t 
 #endif
 }
 
-static const uint8_t popcnt_lookup[16] = { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4 };
-static inline uint32_t popcnt8(const uint32_t x)
-{
-	return popcnt_lookup[x >> 4] + popcnt_lookup[x & 0x0F];
-}
-static inline uint32_t popcnt12(const uint32_t x)
-{
-	return popcnt_lookup[x >> 8] + popcnt8(x & 0xFF);
-}
+static const int8_t imbalance_lookup[16] = { -4, -2, -2, 0, -2, 0, 0, 2, -2, 0, 0, 2, 0, 2, 2, 4 };
 
 static inline int byte_imbalance(uint32_t x)
 {
-	return 2 * popcnt8(x) - 8;
+	return imbalance_lookup[x >> 4] + imbalance_lookup[x & 0xF];
 }
 
-static uint32_t tmds_encode_sym(uint8_t pixel, bool negative)
+static void tmds_encode_symbols(uint8_t pixel, uint32_t* negative_balance_sym, uint32_t* positive_balance_sym)
 {
-	uint32_t popcnt = popcnt8(pixel);
+	int pixel_imbalance = byte_imbalance(pixel);
 	uint32_t sym = pixel & 1;
-	if (popcnt > 4 || (popcnt == 4 && sym == 0)) {
+	if (pixel_imbalance > 0 || (pixel_imbalance == 0 && sym == 0)) {
 		for (int i = 0; i < 7; ++i) {
 			sym |= (~((sym >> i) ^ (pixel >> (i + 1))) & 1) << (i + 1);
 		}
@@ -201,12 +193,18 @@ static uint32_t tmds_encode_sym(uint8_t pixel, bool negative)
 	int imbalance = byte_imbalance(sym & 0xFF);
   if (imbalance == 0) {
 		if ((sym & 0x100) == 0) sym ^= 0x2ff;
+		*positive_balance_sym = sym;
+		*negative_balance_sym = sym;
+		return;
 	}
-	else if (!negative == (imbalance > 0)) {
-		sym ^= 0x2ff;
+	else if (imbalance > 0) {
+		*negative_balance_sym = (sym ^ 0x2ff) | (((-imbalance + imbalance_lookup[2 ^ (sym >> 8)] + 2) & 0x3F) << 26);
+		*positive_balance_sym = sym | ((imbalance + imbalance_lookup[sym >> 8] + 2) << 26);
 	}
-
-	return sym | (((popcnt12(sym) * 2 - 10) & 0x3F) << 26);
+	else {
+		*negative_balance_sym = sym | (((imbalance + imbalance_lookup[sym >> 8] + 2) & 0x3F) << 26);
+		*positive_balance_sym = (sym ^ 0x2ff) | ((-imbalance + imbalance_lookup[2 ^ (sym >> 8)] + 2) << 26);
+	}
 }
 
 // This takes a 16-bit (RGB 565) colour palette and makes palettes of TMDS symbols suitable
@@ -221,12 +219,9 @@ void tmds_setup_palette_symbols(const uint16_t *palette, uint32_t *tmds_palette,
 		uint16_t blue = (palette[i] << 3) & 0xf8;
 		uint16_t green = (palette[i] >> 3) & 0xfc;
 		uint16_t red = (palette[i] >> 8) & 0xf8;
-		tmds_palette_blue[i] = tmds_encode_sym(blue, false);
-		tmds_palette_blue[i + n_palette] = tmds_encode_sym(blue, true);
-		tmds_palette_green[i] = tmds_encode_sym(green, false);
-		tmds_palette_green[i + n_palette] = tmds_encode_sym(green, true);
-		tmds_palette_red[i] = tmds_encode_sym(red, false);
-		tmds_palette_red[i + n_palette] = tmds_encode_sym(red, true);
+		tmds_encode_symbols(blue, &tmds_palette_blue[i], &tmds_palette_blue[i + n_palette]);
+		tmds_encode_symbols(green, &tmds_palette_green[i], &tmds_palette_green[i + n_palette]);
+		tmds_encode_symbols(red, &tmds_palette_red[i], &tmds_palette_red[i + n_palette]);
 	}
 }
 
@@ -242,12 +237,9 @@ void tmds_setup_palette24_symbols(const uint32_t *palette, uint32_t *tmds_palett
 		uint16_t blue = palette[i] & 0xff;
 		uint16_t green = (palette[i] >> 8) & 0xff;
 		uint16_t red = (palette[i] >> 16) & 0xff;
-		tmds_palette_blue[i] = tmds_encode_sym(blue, false);
-		tmds_palette_blue[i + n_palette] = tmds_encode_sym(blue, true);
-		tmds_palette_green[i] = tmds_encode_sym(green, false);
-		tmds_palette_green[i + n_palette] = tmds_encode_sym(green, true);
-		tmds_palette_red[i] = tmds_encode_sym(red, false);
-		tmds_palette_red[i + n_palette] = tmds_encode_sym(red, true);
+		tmds_encode_symbols(blue, &tmds_palette_blue[i], &tmds_palette_blue[i + n_palette]);
+		tmds_encode_symbols(green, &tmds_palette_green[i], &tmds_palette_green[i + n_palette]);
+		tmds_encode_symbols(red, &tmds_palette_red[i], &tmds_palette_red[i + n_palette]);
 	}
 }
 
